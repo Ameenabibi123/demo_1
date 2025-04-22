86802bfb2b2c64145de55c3c12cdadd693efcab8
Ameenabibi123 <ameenamahammadanavar8@gmail.com>
2025-04-22 09:28:52 +0530

    addition prgm

---
+++ addition.java
@@ -0,0 +1,14 @@
+public class addition{
+
+    public static void main(String[] args) {
+
+        int n= 3;
+        int n1 = 9;
+       int result = n + n1;
+       System.out.println("Addition of two numbers is"+ result);
+
+
+
+        
+    }
+}
